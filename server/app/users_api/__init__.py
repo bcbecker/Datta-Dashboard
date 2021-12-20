0020843a53bea8f77ab32592f019f6c42db499ae
Bryce Becker <bryce.becker15@gmail.com>
2021-12-20 16:56:16 -0500

    Added JWT for login
    Added login and update models. Login resource added. Need to handle token storage (Redis?).

--- server/app/users_api/__init__.py
+++ server/app/users_api/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from config import Config
 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class=Config):
@@ -18,6 +20,7 @@ def create_app(config_class=Config):
     # Bind any packages here
     db.init_app(app)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Register any blueprints here
     from .routes import users_api
--- server/app/users_api/routes.py
+++ server/app/users_api/routes.py
@@ -1,14 +1,30 @@
 import uuid
-from flask import request, make_response
+from flask import request
 from flask_restx import Api, Resource, fields
-from . import bcrypt
+from flask_jwt_extended import (
+    create_access_token, get_jwt_identity, jwt_required)
 from .models import User
 
+
 users_api = Api(version="1.0", title="Users API")
+
+
+'''
+Flask-Restx models for associated user request format
+'''
 
 signup_model = users_api.model('SignupModel', {"username": fields.String(required=True, min_length=5, max_length=50),
                                                "email": fields.String(required=True, min_length=5, max_length=100),
                                                "password": fields.String(required=True, min_length=8, max_length=50),
+                                               })
+
+login_model = users_api.model('LoginModel', {"email": fields.String(required=True, min_length=5, max_length=100),
+                                             "password": fields.String(required=True, min_length=8, max_length=50),
+                                             })
+
+update_model = users_api.model('UpdateModel', {"userID": fields.String(required=True, min_length=5, max_length=50),
+                                               "username": fields.String(required=True, min_length=5, max_length=50),
+                                               "email": fields.String(required=True, min_length=5, max_length=100)
                                                })
 
 
@@ -32,14 +48,43 @@ class Signup(Resource):
                 public_id=str(uuid.uuid4()),
                 username=_username,
                 email=_email,
-                password=bcrypt.generate_password_hash(_password),
                 jwt_auth=False
             )
 
+            new_user.set_password(_password)
             new_user.save_user()
 
-            return ({'user': new_user.email,
-                     'msg': 'Successfully registered.'}, 201)
+            return ({'msg': 'Successfully registered.',
+                     'user': new_user.to_JSON(),
+                     }, 201)
         else:
-            # returns 202 if user already exists
-            return make_response('User already exists. Please Log in.', 202)
+            return ({'msg': 'User already exists!'}, 202)
+
+
+@users_api.route('/api/users/login')
+class Login(Resource):
+    '''
+    Takes login_model as input and logs user in with JWT
+    '''
+    @users_api.expect(login_model)
+    def post(self):
+
+        request_data = request.form
+        _email = request_data.get('email')
+        _password = request_data.get('password')
+
+        user = User.query.filter_by(email=_email).first()
+
+        if not user:
+            return ({'msg': 'User not found'}, 401)
+
+        if user.check_password(_password):
+            token = create_access_token(identity=user.email)  # should be public_id
+            user.jwt_auth = True
+            user.save_user()
+
+            return ({'user': user.to_JSON(),
+                     'token': token,
+                     'msg': 'Successfully logged in.'}, 200)
+
+        return ({'msg': 'Could not verify credentials'}, 403)

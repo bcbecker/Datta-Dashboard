4d64876781607293c1aae01b22e6fad9edb3ed11
Bryce Becker <bryce.becker15@gmail.com>
2021-12-21 17:11:19 -0500

    Added JWT secret to config
    Will now use this for encoding tokens instead of the flask secret key

--- server/config.py
+++ server/config.py
@@ -11,6 +11,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_TOKEN_LOCATION = ['headers']
     JWT_COOKIE_SECURE = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)

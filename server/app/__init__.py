697b3dc008ff2d638d63978a0f6fef84b8601257
Bryce Becker <bryce.becker15@gmail.com>
2021-12-21 17:12:35 -0500

    Docstring formatting

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,6 @@
 #For packaging
 
-#Houses all the resources/APIs needed in this particular app. Allows the addition of
-# other APIs easily, simply adding the module and updating necessary binds/configs
+'''
+Houses all the resources/APIs needed in this particular app. Allows the addition of
+other APIs easily, simply adding the module and updating necessary binds/configs
+'''
